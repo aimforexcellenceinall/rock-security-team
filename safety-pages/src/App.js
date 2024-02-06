9a22b58ac186bdb58d12e54d341a7edce079705c
JOSEJ070 <Jq.jose070@gmail.com>
2024-02-05 22:11:55 -0800

    Update firebase-setup-and-docs

--- safety-pages/src/App.js
+++ safety-pages/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { AppContext } from './components/context/AppContext';
-import { auth, database, analytics } from './components/Firebase';
-import ErrorBoundary from './components/common/ErrorBoundary';
-import Register from './components/forms/Register';
-import Login from './components/forms/Login';
-import HomePage from './components/pages/HomePage';
-import LostFoundChildForm from './components/forms/LostFoundChildForm';
-import ParentIsLostForm from './components/forms/ParentIsLostForm';
-import PromptPage from './components/pages/PromptPage';
-import NotepadPage from './components/pages/NotepadPage'; 
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { AppContext } from "./context/AppContext";
+import { auth, database, analytics } from "./services/firebase/FirebaseConfig";
+import { appCheck } from "./services/firebase/AppCheck";
+import ErrorBoundary from "./components/common/ErrorBoundary";
+import HomePage from "./components/pages/HomePage";
+import Login from "./components/forms/Login";
+import LostFoundChildForm from "./components/forms/LostFoundChildForm";
+import NotepadPage from "./components/pages/NotepadPage";
+import ParentIsLostForm from "./components/forms/ParentIsLostForm";
+import PromptPage from "./components/pages/PromptPage";
+import Register from "./components/forms/Register";
+import "./App.css";
 
 function App() {
   return (
@@ -19,34 +19,48 @@ function App() {
       <Router>
         <div className="app-container">
           <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/notepad">Notepad</Link></li>
-            <li><Link to="/prompt">Prompt Page</Link></li>
-            <li><Link to="/report-child">Lost & Found Child Form</Link></li>
-            <li><Link to="/report-parent">Lost & Found Parent Form</Link></li>
-          </ul>
-        </nav>
-        <div className="content-area">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/notepad" element={<NotepadPage />} />
-          <Route path="/prompt" element={<PromptPage />} />
-          <Route path="/report-child" element={<LostFoundChildForm />} />
-          <Route path="/report-parent" element={<ParentIsLostForm />} />
-        </Routes>
-        </div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/notepad">Notepad</Link>
+              </li>
+              <li>
+                <Link to="/prompt">Prompt Page</Link>
+              </li>
+              <li>
+                <Link to="/report-child">Lost & Found Child Form</Link>
+              </li>
+              <li>
+                <Link to="/report-parent">Lost & Found Parent Form</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="content-area">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/notepad" element={<NotepadPage />} />
+              <Route path="/prompt" element={<PromptPage />} />
+              <Route path="/report-child" element={<LostFoundChildForm />} />
+              <Route path="/report-parent" element={<ParentIsLostForm />} />
+            </Routes>
+          </div>
 
-<div className="notepad-panel">
-  <NotepadPage />
-</div>
-</div>
-</Router>
-</ErrorBoundary>
+          <div className="notepad-panel">
+            <NotepadPage />
+          </div>
+        </div>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
---
+++ safety-pages/src/services/firebase/AppCheck.js
@@ -0,0 +1,6 @@
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(process.env.REACT_APP_RECAPTCHA_KEY),
+  isTokenAutoRefreshEnabled: true
+});
